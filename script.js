42d33137ea3aa2d112a844d054af4bae1fb52f14
mary <marynyamburah321@gmail.com>
2023-05-29 12:09:10 +0300

    Calculator using Javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.clear');
+    let equals = document.querySelector('.equal');
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function(e) {
+        let value = e.target.dataset.num;
+        screen.value += value;
+      });
+    });
+  
+    equals.addEventListener('click', function() {
+      try {
+        let result = eval(screen.value);
+        screen.value = result;
+      } catch (error) {
+        screen.value = 'Error';
+      }
+    });
+  
+    clear.addEventListener('click', function() {
+      screen.value = '';
+    });
+  });
+  
